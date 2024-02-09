f218f6c9be71168722858646ba6f8860520345e7
John Dorsey <127167614+JohnDoe4991@users.noreply.github.com>
2024-02-08 20:28:14 -0800

    Initial Commit

---
+++ Restaurant/RestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantApp.swift
+//  Restaurant
+//
+//  Created by John Tejada on 2/8/24.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
