12e9619a1d3fed82c6726f1294e26e6bbed1082d
John Dorsey <127167614+JohnDoe4991@users.noreply.github.com>
2024-02-09 20:15:48 -0800

    signinapple

--- Restaurant/ContentView.swift
+++ Restaurant/ContentView.swift
@@ -1,14 +1,65 @@
+import AuthenticationServices
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.colorScheme) var colorScheme
+    
+    @AppStorage("email") var email: String = ""
+    @AppStorage("firstName") var firstName: String = ""
+    @AppStorage("lastName") var lastName: String = ""
+    @AppStorage("userId") var userId: String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world1!")
+        NavigationView {
+            VStack {
+                
+                if userId.isEmpty {
+                    SignInWithAppleButton(.continue) {request in
+                        
+                        request.requestedScopes = [.email, .fullName]
+                        
+                    } onCompletion: { result in
+                        
+                        switch result {
+                        case .success(let auth):
+                            
+                            switch auth.credential {
+                            case let credential as ASAuthorizationAppleIDCredential:
+                                
+                                // User Id
+                                let userId = credential.user
+                                
+                                // User Info
+                                let email = credential.email
+                                let firstName = credential.fullName?.givenName
+                                let lastName = credential.fullName?.familyName
+                                
+                                self.email = email ?? ""
+                                self.userId = userId
+                                self.firstName = firstName ?? ""
+                                self.lastName = lastName ?? ""
+                                
+                                break
+                            default:
+                                break
+                            }
+                            
+                        case .failure(let error):
+                            print(error)
+                        }
+                    }
+                    .signInWithAppleButtonStyle(
+                        colorScheme == .dark ? .white : .black)
+                    .frame(height:50 )
+                    .padding()
+                    .cornerRadius(8)
+                }
+                else {
+                    // signed in 
+                }
+            }
+            .navigationTitle("Sign In")
         }
-        .padding()
     }
 }
 
