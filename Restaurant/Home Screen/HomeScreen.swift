5433e9d0fc5b5a814f56193f05d6420a07046e6e
John Dorsey <127167614+JohnDoe4991@users.noreply.github.com>
2024-02-12 16:53:19 -0800

    location

--- Restaurant/Home Screen/HomeScreen.swift
+++ Restaurant/Home Screen/HomeScreen.swift
@@ -6,11 +6,55 @@
 //
 
 import SwiftUI
+import MapKit
+import CoreLocationUI
 
 struct HomeScreen: View {
     
+    @StateObject private var viewModel = HomeViewModel()
+
     var body: some View {
-        Text("Welcome to the Main Screen!")
+        ZStack (alignment: .bottom) {
+            Map(coordinateRegion: $viewModel.region, showsUserLocation: true)
+                .ignoresSafeArea()
+                .tint(.pink)
+            
+            LocationButton(.currentLocation) {
+                viewModel.requestAllowOnceLocationPermission()
+            }
+            .foregroundColor(.white)
+            .cornerRadius(8)
+            .labelStyle(.titleAndIcon)
+            .padding(.bottom, 50)
+        }
+    }
+}
+
+final class HomeViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
+    @Published var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.828724, longitude: -122.355537), span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1))
+    let locationManager = CLLocationManager()
+    
+    override init() {
+        super.init()
+        locationManager.delegate = self
+    }
+    
+    func requestAllowOnceLocationPermission() {
+        locationManager.requestLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let latestLocation = locations.first else {
+            print("Error")
+            return
+        }
+        
+        DispatchQueue.main.async {
+            self.region = MKCoordinateRegion(center: latestLocation.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
+        }
+    }
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error.localizedDescription)
     }
 }
 
