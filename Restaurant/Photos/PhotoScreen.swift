8ece89344ab1f857c2ffd26d43e8d9e46a5d8647
John Dorsey <127167614+JohnDoe4991@users.noreply.github.com>
2024-02-13 19:45:16 -0800

    start togglescreen

--- Restaurant/Photos/PhotoScreen.swift
+++ Restaurant/Photos/PhotoScreen.swift
@@ -9,50 +9,49 @@ import SwiftUI
 import PhotosUI
 
 struct PhotoScreen: View {
-    @State private var avatarImage: UIImage?
-    @State private var photoPickerItem: PhotosPickerItem?
+    @State private var images: [UIImage] = []
+    @State private var photoPickerItems: [PhotosPickerItem] = []
     
     var body: some View {
         VStack{
-            HStack(spacing:20){
-                PhotosPicker(selection: $photoPickerItem, matching: .images) {
-                    Image(uiImage: avatarImage ?? UIImage(systemName: "person.crop.circle")!)
-                        .resizable()
-                        .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
-                        .frame(width:100, height: 100)
-                        .clipShape(.circle)
-                }
-                
-                VStack(alignment: .leading) {
-                    Text("New User")
-                        .font(.largeTitle.bold())
-                    
-                    Text("iOS Developer")
-                        .font(.headline)
-                        .foregroundStyle(.secondary)
-                }
-                
-                Spacer()
-            }
+            PhotosPicker("Select Photos", selection: $photoPickerItems, maxSelectionCount: 5, selectionBehavior: .ordered)
             
-            Spacer()
+            ScrollView(.horizontal) {
+                    HStack(spacing:20){
+                        ForEach(0..<images.count, id: \.self) {i in
+                        Image(uiImage: images[i])
+                            .resizable()
+                            .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                            .frame(width:100, height: 100)
+                            .clipShape(.circle)
+                    }
+                }
+            }
         }
         .padding(30)
-        .onChange(of: photoPickerItem) { newItem in
+        .onChange(of: photoPickerItems) { newItem in
             Task {
-                if let photoPickerItem = newItem,
-                   let data = try? await photoPickerItem.loadTransferable(type: Data.self) {
-                    if let image = UIImage(data: data) {
-                        avatarImage = image
+                for item in photoPickerItems {
+                    if let data = try? await item.loadTransferable(type: Data.self) {
+                        if let image = UIImage(data: data) {
+                            images.append(image)
+                        }
                     }
                 }
                 
-                self.photoPickerItem = nil // Reset the picker item
+                self.photoPickerItems.removeAll() // Reset the picker item
             }
         }
-
-    }
-}
+        .navigationTitle("Photos")
+                    .toolbar {
+                        ToolbarItem(placement: .navigationBarTrailing) {
+                            NavigationLink(destination: ToggleScreen()) {
+                                Text("Next")
+                            }
+                        }
+                    }
+                }
+            }
 
 #Preview {
     PhotoScreen()
---
+++ Restaurant/ToggleScreen/ToggleScreen.swift
@@ -0,0 +1,18 @@
+//
+//  ToggleScreen.swift
+//  Restaurant
+//
+//  Created by John Tejada on 2/13/24.
+//
+
+import SwiftUI
+
+struct ToggleScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ToggleScreen()
+}
